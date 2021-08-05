b9a6111d858cc9cf56894e9dcef495043f0013cb
Gaurav <fillic2002@gmail.com>
2021-08-05 18:17:41 +0530

    Pulish latest ELK setup

---
+++ ELKClient/ELKClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELKClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Elkhelper elk = new Elkhelper();
+			elk.IndexDocument();
+			elk.IndexBulk();
+		}
+	}
+}
---
+++ ELKClient/ELKClient/elkhelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace ELKClient
+{
+	public class Elkhelper
+	{
+		ConnectionSettings settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+			.DefaultIndex("book");
+		public void IndexDocument()
+		{
+			try
+			{				
+				ElasticClient client = new ElasticClient(settings);
+				Book bkObj = new Book() { id=3,author = "Manjunath", name = "Finance education" };
+				var s=client.IndexDocument<Book>(bkObj);
+			}
+			catch(Exception ex)
+			{
+				var s = ex.StackTrace;
+			}
+		}
+		public void IndexBulk()
+		{
+			//var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+			//			.DefaultIndex("book");
+			IList<Book> b = new List<Book>();
+			b.Add(new Book() { id = 3, author = "ABC", name = "elastic fundamental" });
+			b.Add(new Book() { id = 3, author = "DEF", name = "elastic fundamental" });
+			b.Add(new Book() { id = 3, author = "GHH", name = "elastic fundamental" });
+
+			ElasticClient client = new ElasticClient(settings);
+
+			var bulkIndexResponse = client.Bulk(x => x
+				.Index("book")
+				.IndexMany(b)
+				);
+		}
+	}
+
+	public class Book
+	{
+		public int id;
+		public string name;
+		public string author;
+	}
+}
